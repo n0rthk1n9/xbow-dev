41799a41ded44e660f6c73f5f2674502ec9c665b
Jan Armbrust <jan.armbrust@gmx.de>
2019-11-11 21:34:44 +0100

    add background color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { Emoji } from './components/UI/Emoji/Emoji';
+import { Card } from './components/UI/Card/Card';
 import classes from './App.module.css';
 
 function App() {
   return (
     <div className={classes.App}>
-      <span>Hello World</span>
-      <Emoji symbol="🚀" label="rocket" />
+      <Card>
+        <span>Hello World</span>
+        <Emoji symbol="🚀" label="rocket" />
+      </Card>
     </div>
   );
 }
