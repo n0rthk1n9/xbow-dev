db882f66cc8a0b254942f75132877740d35aa1ad
Jan Armbrust <jan.armbrust@gmx.de>
2020-04-18 17:51:30 +0200

    add home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Emoji } from '../../components/UI/Emoji/Emoji';
+import { Card } from '../../components/UI/Card/Card';
+import classes from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={classes.Home}>
+      <Card>
+        <span>Hello World</span>
+        <Emoji symbol="🚀" label="rocket" />
+      </Card>
+      <Card>
+        <span>automatic deployment working</span>
+        <Emoji symbol="😍" label="heart_eyed_face" />
+      </Card>
+    </div>
+  );
+}
+
+export { Home };
