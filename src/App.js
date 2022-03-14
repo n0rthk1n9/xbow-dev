a363a2d9810ee0e8de27a6c3eb144b5c2b9ff3f4
Jan Armbrust <jan.armbrust@googlemail.com>
2022-03-14 16:30:40 +0100

    add streaks app support page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Privacy } from './pages/Privacy/Privacy';
 import { ChameleonCalculator } from './pages/ChameleonCalculator/ChameleonCalculator';
+import { Streaks } from './pages/Streaks/Streaks'
 
 function App() {
   return (
     <Switch>
       <Route path="/chameleon-calculator" component={ChameleonCalculator} />
+      <Route path="/streaks" component={Streaks} />
       <Route path="/privacy" component={Privacy} />
       <Route path="/" exact component={Home} />
       <Redirect to="/" />
