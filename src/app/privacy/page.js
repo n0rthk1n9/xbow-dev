cbfc49650ad2ab8151ec62a59b0c4a26ea84a4c5
Jan Armbrust <jan.armbrust@googlemail.com>
2025-05-19 17:00:03 +0200

    Feature: Cursor play around (#38)
    * Major redesign with cursor
    
    * Fix build errors
    
    * Fix node version
    
    * Fix no escaped characters rule
    
    * Fix old code removal
    
    * Add netlify config

---
+++ src/app/privacy/page.js
@@ -0,0 +1,96 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function Privacy() {
+  return (
+    <main className="container mx-auto min-h-screen px-4 py-8 md:px-8 lg:px-16">
+      <div className="mx-auto max-w-3xl space-y-6">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="card"
+        >
+          <div className="space-y-6">
+            <h1 className="text-2xl font-bold text-text-light dark:text-text-dark">
+              Privacy Policy
+            </h1>
+            <div className="space-y-4 text-text-secondary-light dark:text-text-secondary-dark">
+              <p>
+                Jan Armbrust built these apps as Free apps. These SERVICES are provided by Jan Armbrust at no cost and are intended for use as is.
+              </p>
+              <p>
+                This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Services.
+              </p>
+              <p>
+                If you choose to use my Services, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Services. I will not use or share your information with anyone except as described in this Privacy Policy.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Information Collection and Use</h2>
+              <p>
+                For a better experience, while using our Services, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Log Data</h2>
+              <p>
+                I want to inform you that whenever you use my Services, in a case of an error in the app I collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol ("IP") address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Cookies</h2>
+              <p>
+                Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
+              </p>
+              <p>
+                These Services do not use these "cookies" explicitly. However, the apps may use third party code and libraries that use "cookies" to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of these Services.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Service Providers</h2>
+              <p>I may employ third-party companies and individuals due to the following reasons:</p>
+              <ul className="list-disc pl-6 space-y-2">
+                <li>To facilitate our Services;</li>
+                <li>To provide the Services on our behalf;</li>
+                <li>To perform Service-related services; or</li>
+                <li>To assist us in analyzing how our Services are used.</li>
+              </ul>
+              <p>
+                I want to inform users of these Services that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Security</h2>
+              <p>
+                I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Links to Other Sites</h2>
+              <p>
+                These Services may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Children's Privacy</h2>
+              <p>
+                These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do necessary actions.
+              </p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Changes to This Privacy Policy</h2>
+              <p>
+                I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
+              </p>
+              <p>This policy is effective as of 2023-03-13</p>
+
+              <h2 className="text-xl font-semibold text-text-light dark:text-text-dark">Contact Us</h2>
+              <p>
+                If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at{' '}
+                <a href="mailto:rabble-oceans05@icloud.com" className="text-primary hover:underline">
+                  rabble-oceans05@icloud.com
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </main>
+  );
+} 
